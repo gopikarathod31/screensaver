8b59d122ac9c40778ffc195a3ebef37281e76a07
Gopika <Gopika.multiqos@gmail.com>
2025-02-05 11:52:50 +0530

    Initial Commit

---
+++ TimeScreenSaver/TimeScreenSaverView.h
@@ -0,0 +1,12 @@
+//
+//  TimeScreenSaverView.h
+//  TimeScreenSaver
+//
+//  Created by MQS_2 on 05/02/25.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface TimeScreenSaverView : ScreenSaverView
+
+@end
---
+++ TimeScreenSaver/TimeScreenSaverView.m
@@ -0,0 +1,51 @@
+//
+//  TimeScreenSaverView.m
+//  TimeScreenSaver
+//
+//  Created by MQS_2 on 05/02/25.
+//
+
+#import "TimeScreenSaverView.h"
+
+@implementation TimeScreenSaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
